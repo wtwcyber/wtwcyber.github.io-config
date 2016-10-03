b54f16e9e93952d6ef5d832cc8b2e1dcbff23c6c
taylan <taylan@xpshost.com>
2016-10-03 13:04:50 -0700

    updating site try 2

--- update-live-site.sh
+++ update-live-site.sh
@@ -1,10 +1,10 @@
 hugo
-git add --all .
+git add .
 git commit -m "Update site config `date`"
 git push -u origin master
 rm -rf live_site/*
 cp -r public/* live_site/
 cd live_site/
-git add --all .
+git add .
 git commit -m "Update site content `date`"
 git push -u origin master
