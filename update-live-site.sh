95f3f50de7afc150e55973f833b4a014ea0a800e
Sam Scheele <sam.scheele@outlook.com>
2016-10-04 17:58:21 -0400

    Added not the theme

---
+++ update-live-site.sh
@@ -0,0 +1,12 @@
+git pull
+hugo
+git add .
+git commit -m "Update site config `date`"
+git push -u origin master
+rm -rf live_site/*
+cp -r public/* live_site/
+cd live_site/
+git pull
+git add .
+git commit -m "Update site content `date`"
+git push -u origin master
